fcd4b061caa617eadf35f60da9333a3ac967c8b7
volca <volca@tom.com>
2015-07-09 17:42:11 +0800

    add dht11Logger.ino

--- libraries/BlueDuino/AB_BLE.cpp
+++ libraries/BlueDuino/AB_BLE.cpp
@@ -6,18 +6,27 @@ int AB_BLE::available() {
     return stream->available();
 }
 
-void AB_BLE::write(unsigned char dat) {
-    stream->write(dat);
+size_t AB_BLE::write(uint8_t c) {
+    return stream->write(c);
 }
 
-void AB_BLE::writeBytes(unsigned char *dat, unsigned char len) {
+void AB_BLE::writeBytes(unsigned char *data, unsigned char len) {
     delay(10);
     if(len > 0) {
-        stream->write(dat, len);
+        stream->write(data, len);
     }
+}
+
+void AB_BLE::println(const char data[]) {
+    stream->println(data);
+}
+
+size_t AB_BLE::println(int n, int base) {
+    stream->println(n, base);
 }
 
 int AB_BLE::read() {
     delay(10);	
     return stream->read();
 }
+
